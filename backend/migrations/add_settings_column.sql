321e108f9a4d3e8407befd364c249ac9765870f7
Sachita92 <sachitasigdel0713@gmail.com>
2026-06-02 22:08:44 +0545

    Added Procfile for Railway deployment

--- backend/migrations/add_settings_column.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE stores
-  ADD COLUMN IF NOT EXISTS settings JSONB DEFAULT '{}'::jsonb;
-
--- Verify the column was added:
-SELECT column_name, data_type
-FROM information_schema.columns
-WHERE table_name = 'stores'
-ORDER BY ordinal_position;
